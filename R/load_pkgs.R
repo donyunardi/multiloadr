052cf327652084ffa0080e7d02d393a9b91c2ade
Dony Unardi <donyunardi@gmail.com>
2023-04-05 23:53:59 -0700

    closes #3, update man

--- R/load_pkgs.R
+++ R/load_pkgs.R
@@ -1,30 +1,61 @@
-#' Load packages listed in the multiloadr option
+#' Load packages and switch git branch if specified
 #'
-#' This function loads packages listed in the multiloadr option using the
-#' \code{devtools::load_all()} function.
+#' This function loads packages specified in the \code{multiloadr} option and
+#' switches to a specified git branch if provided.
 #'
-#' @examples
-#' load_pkgs()
+#' @param branch_name Character string specifying the name of the git branch to
+#' switch to. Defaults to \code{NULL}.
+#'
+#' @return This function returns nothing, but prints a message indicating which
+#' packages were loaded and from which branch.
 #'
 #' @export
-load_pkgs <- function() {
+#'
+#' @examples
+#' # Load packages without switching branch
+#' load_pkgs()
+#'
+#' # Load packages and switch to "develop" branch
+#' load_pkgs("develop")
+load_pkgs <- function(branch_name = NULL) {
+
   pkgs <- getOption("multiloadr", NULL)
+
   if (is.null(pkgs)) {
     message("No packages to load")
     invisible()
   }
+
   for (i in seq_along(pkgs)) {
+
     pkg <- names(pkgs)[i]
     path <- pkgs[[i]]
-    branch <- system(
-      paste("cd", path, "&& git branch --show-current"), intern = TRUE
+
+    if (!is.null(branch_name)) {
+      change_branch <- system2(
+        "cd",
+        args = c(path, paste("&& git checkout", branch_name)),
+        stdout = FALSE,
+        stderr = FALSE
+      )
+      if (change_branch == 1) {
+        message(paste(branch_name, "branch doesn't exist in", pkg))
+      }
+    }
+
+    branch <- system2(
+      "cd",
+      args = c(path, "&& git branch --show-current"),
+      stdout = TRUE
     )
+
     cat(
       sprintf(
         "Load \033[0;94m%s\033[0m from the \033[0;92m%s\033[0m branch.\n",
         pkg, branch
       )
     )
+
     load_all(path)
   }
 }
