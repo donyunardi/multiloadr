a8779782ad35a8dbe6e6e732796f7a5aa03e886f
Dony Unardi <donyunardi@gmail.com>
2023-04-21 00:13:35 -0700

    fix #7 allow multiple branch_name

--- R/load_pkgs.R
+++ R/load_pkgs.R
@@ -1,4 +1,4 @@
-#' Load packages and switch git branch if specified
+#' Load packages from specified branch and perform git pull if specified
 #'
 #' This code loads packages based on the multiloadr option provided. It has an
 #' additional feature that allows loading packages from a particular branch if
@@ -6,12 +6,16 @@
 #' branch. The function also provides an optional argument to execute a git pull
 #' before loading the packages.
 #'
-#' @param branch_name Character string specifying the name of the git branch to
-#' switch to. Defaults to \code{NULL}. In case the branch is NULL or
-#' non-existent, it will be loaded from the presently active branch.
+#' The function accepts multiple branch_name arguments, but only the first
+#' branch that is found will be loaded for each package.
 #'
-#' @param git_pull A logical value indicating whether to perform a `git pull``
-#' before loading the package.
+#' @param branch_name Character vector specifying the name of the git branch(es)
+#' to load. Defaults to \code{NULL}. The first branch that is found will be
+#' loaded for each package. If it is NULL or non-existent, it will be loaded
+#' from the presently active branch.
+#'
+#' @param git_pull A logical value (TRUE/FALSE) indicating whether to perform a
+#' `git pull` before loading the package. Defaults to \code{FALSE}.
 #'
 #' @return This function returns nothing, but prints a message indicating which
 #' packages were loaded and from which branch.
@@ -22,8 +26,16 @@
 #' # Load packages without switching branch
 #' load_pkgs()
 #'
-#' # Load packages from "develop" branch
-#' load_pkgs("develop")
+#' # Switch to "develop" branch and load packages
+#' load_pkgs(branch_name = "develop")
+#'
+#' # Switch to the "develop" branch, pull the latest changes, and load packages
+#' load_pkgs(branch_name = "develop", git_pull = TRUE)
+#'
+#' # Load packages from "develop" branch if available, or "main" branch otherwise
+#' # Pull the latest changes before loading the packages
+#' load_pkgs(branch_name = c("develop", "main"), git_pull = TRUE)
+#'
 load_pkgs <- function(branch_name = NULL, git_pull = FALSE) {
 
   pkgs <- get_multiloadr_pkgs()
@@ -38,15 +50,30 @@ load_pkgs <- function(branch_name = NULL, git_pull = FALSE) {
     pkg <- names(pkgs)[i]
     path <- pkgs[[i]]
 
+    get_pkg_branches <- system2(
+      "cd",
+      args = c(path, paste("&& git branch")),
+      stdout = TRUE
+    )
+
+    get_pkg_branches <- trimws(gsub("^\\*", "", get_pkg_branches))
+
     if (!is.null(branch_name)) {
-      change_branch <- system2(
-        "cd",
-        args = c(path, paste("&& git checkout", branch_name)),
-        stdout = FALSE,
-        stderr = FALSE
-      )
-      if (change_branch == 1) {
-        cat(paste(branch_name, "branch doesn't exist in", pkg, "\n"))
+      for (branch in branch_name) {
+        if (branch %in% get_pkg_branches) {
+          cat(paste0("\033[0;92m", branch, " branch exist in ", pkg, "\033[0m\n"))
+
+          system2(
+            "cd",
+            args = c(path, paste("&& git checkout", branch)),
+            stdout = FALSE,
+            stderr = FALSE
+          )
+
+          break
+        } else {
+          cat(paste0("\033[0;91m", branch, " branch doesn't exist in ", pkg, "\033[0m\n"))
+        }
       }
     }
 
@@ -73,8 +100,8 @@ load_pkgs <- function(branch_name = NULL, git_pull = FALSE) {
       )
 
       if (!check_remote) {
-        cat("\033[0;92mRemote URL exists...\033[0m\n")
-        cat("\033[0;92mPerforming git pull...\033[0m\n")
+        cat("\033[0;96mRemote URL exists...\033[0m\n")
+        cat("\033[0;96mPerforming git pull...\033[0m\n")
         system2(
           "cd",
           args = c(path, "&& git pull"),
